2077fba219189651f0d26063df41fd55a5736c70
Koraim Wesam <wesam.koraim@aalto.fi>
2019-10-18 18:58:11 +0300

    Adding Universities Projects

--- simple_ic_tester.h
+++
@@ -1,8 +0,0 @@
-#include <reg52.h>
-
-unsigned int test_7411();
-unsigned int test_7486();
-unsigned int test_7408();
-unsigned int test_7432();
-unsigned int test_7400();
-unsigned int test_7404();
